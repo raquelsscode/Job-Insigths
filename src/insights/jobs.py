d1819284fa618ca92bdc0c54eef7ae3058160141
Raquel Santos da Silva <raquelsantossilva2405@outlook.com>
2023-02-16 20:42:13 -0300

    requisito 2

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -15,21 +15,13 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
+    jobs = read(path)
+    
+    job_types = []
+    for job in jobs:
+        job_types.append(job["job_type"])
+    return set(job_types)
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
