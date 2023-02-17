d8838c8b83d53df7fb403ee2d087818c537a23d1
Raquel Santos da Silva <raquelsantossilva2405@outlook.com>
2023-02-16 23:43:20 -0300

    Requisito 9

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -22,30 +22,22 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
        or not float(str(job["max_salary"])).is_integer()):
         # Seção 2 do site: https://bityli.com/1HsaL
         raise ValueError("min_salary or max_salary aren't valid integers")
-    if job["min_salary"] > job["max_salary"]:
+    if int(job["min_salary"]) > int(job["max_salary"]):
         raise ValueError("min_salary is greather than max_salary")
-    if not float(str(salary)).is_integer():
+    if not str(salary).replace("-", "").isnumeric():
         raise ValueError("salary isn't a valid integer")
 
     return int(job["min_salary"]) <= int(salary) <= int(job["max_salary"])
 
 
 def filter_by_salary_range(
-    jobs: List[dict],
-    salary: Union[str, int]
+    jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    jobs_list = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                jobs_list.append(job)
+        except ValueError as exc:
+            print(exc)
+    return jobs_list
